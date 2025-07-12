7c96bb1bf47aac45460e14d0281a7d92f9795937
yash kumar <you@example.com>
2025-07-12 11:45:49 +0530

    relational operator

---
+++ 04_relational_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2;
+    cout << "enter first number=";
+    cin >> n1;
+    cout << "enter second number=";
+    cin >> n2;
+    cout << (n1 > n2) << endl;
+    cout << (n1 >= n2) << endl;
+    cout << (n1 < n2) << endl;
+    cout << (n1 <= n2) << endl;
+    cout << (n1 == n2) << endl;
+    cout << (n1 != n2) << endl;
+    return 0;
+}
