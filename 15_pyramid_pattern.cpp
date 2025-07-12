7f1514b9dee6967c0834eb9c7c19e78627bc67e1
yash kumar <you@example.com>
2025-07-12 11:50:10 +0530

    pyramid pattern

---
+++ 15_pyramid_pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter no of row=";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << j;
+        }
+        for (int j = i; j > 0; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
