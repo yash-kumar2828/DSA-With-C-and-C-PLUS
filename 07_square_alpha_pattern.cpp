4c69c9e4745d654377c8d26051a6ba752860ea13
yash kumar <you@example.com>
2025-07-12 11:47:01 +0530

    square alphabet pattern

---
+++ 07_square_alpha_pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter no of row=";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << (char)(j + 64) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
