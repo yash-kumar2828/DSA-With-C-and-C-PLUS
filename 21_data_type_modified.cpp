8a11b5b46fd25be8ac649f391b9e8aff1d438ec5
yash kumar <you@example.com>
2025-07-12 11:52:33 +0530

    data type modified

---
+++ 21_data_type_modified.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "size of int =" << sizeof(int) << endl;
+    cout << "size of long int =" << sizeof(long int) << endl;
+    cout << "size of short int =" << sizeof(short int) << endl;
+    cout << "size of long long =" << sizeof(long long) << endl;
+    cout << "size of signed int =" << sizeof(signed int) << endl;
+    cout << "size of unsigned int =" << sizeof(unsigned int) << endl;
+    cout << "size of char =" << sizeof(char) << endl;
+    cout << "size of float =" << sizeof(float) << endl;
+    return 0;
+}
