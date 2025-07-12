2db9133655982834ddb36a00c452f4446fc1cb5b
yash kumar <you@example.com>
2025-07-12 11:43:53 +0530

    area of square

---
+++ 01_area_of_square.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float side;
+    cout << "enter a area of side=";
+    cin >> side;
+    cout << "area of square is " << side * side;
+    return 0;
+}
